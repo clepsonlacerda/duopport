7115567806ce6ee4e7bb85823966f66cdd3f373d
Clepson <clepsonsantos9@gmail.com>
2024-03-31 21:54:12 -0300

    feat: add section page

---
+++ src/app/section/page.tsx
@@ -0,0 +1,16 @@
+import Header from "../_components/header";
+
+const Section = () => {
+  return (
+    <section>
+      <Header />
+
+      <div className="container mt-8">
+
+        seção
+      </div>
+    </section>
+  );
+}
+
+export default Section;
